085981058ddaef87ca11e50279e3471a04d02c45
Vivien Klaousen <vivienklaousen@MacBook-Pro-de-Vivien.local>
2016-11-09 10:37:55 +0100

    The tweet are displayed in real time on the interface with socket.io.

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var ent = require('ent');
+var twit = require('twitter');
+
+twitter = new twit({
+    consumer_key: '3GfegpTIRmndaEHwJcDX9e4j5',
+    consumer_secret: 'rTQxCS17P9r4a9lrE7EMO3KA7jEnufiD3LO7p7wTLNxmC9g04D',
+    access_token_key: '2776305215-bJiqHRwcEwh5O8J8TUe1n6sWu6vU2Di9XdsLVO6',
+    access_token_secret: '6dEu1vMXIuALwuyU93FTcWRv3FaWItXgMsC1WBmpzVGfa'
+});
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -17,7 +25,6 @@ io.sockets.on('connection', function(socket) {
     }
 
     socket.on('login', function(user){
-        console.log(user);
         pseudo = user;
         users [pseudo.user] = user;
         socket.emit('logged');
@@ -30,6 +37,24 @@ io.sockets.on('connection', function(socket) {
         }
         delete users[pseudo.username];
         io.sockets.emit('decoUser', pseudo);
+    });
+
+    twitter.stream('statuses/filter', { track: '#Clinton' },
+        function(stream) {
+ 
+        stream.on('data', function( tweet ) {
+            var tweet_id = tweet.id_str;
+            var tweet_text = tweet.text;
+            var name = tweet.user.name;
+            console.log(name);
+            console.log(tweet_text);
+            socket.emit('tweet', tweet);
+        });
+ 
+        stream.on('error', function ( error ) {
+            console.error(error);
+        });
+ 
     });
 
 });
