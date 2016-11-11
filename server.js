7bca5e47ae90f8089c123a73fa8c446ca287ed53
Vivien Klaousen <vivienklaousen@MacBook-Pro-de-Vivien.local>
2016-11-11 01:09:16 +0100

    The tweet for all user

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+/**
+*
+**/
+
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var ent = require('ent');
 var twit = require('twitter');
+
+/**
+*
+**/
 
 twitter = new twit({
     consumer_key: '3GfegpTIRmndaEHwJcDX9e4j5',
@@ -11,11 +19,23 @@ twitter = new twit({
     access_token_secret: '6dEu1vMXIuALwuyU93FTcWRv3FaWItXgMsC1WBmpzVGfa'
 });
 
+/**
+*
+**/
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+/**
+*
+**/
+
 var users = {};
+
+/**
+*
+**/
 
 io.sockets.on('connection', function(socket) {
     var pseudo = false;
@@ -24,12 +44,20 @@ io.sockets.on('connection', function(socket) {
         socket.emit('newuser', users[k]);
     }
 
+    /**
+    *
+    **/
+
     socket.on('login', function(user){
         pseudo = user;
         users [pseudo.user] = user;
         socket.emit('logged');
         socket.broadcast.emit('newuser', pseudo);
     });
+
+    /**
+    *
+    **/
 
     socket.on('disconnect', function(){
         if (!pseudo){
@@ -39,16 +67,17 @@ io.sockets.on('connection', function(socket) {
         io.sockets.emit('decoUser', pseudo);
     });
 
-    twitter.stream('statuses/filter', { track: '#Clinton' },
+    /**
+    *
+    **/
+
+    twitter.stream('statuses/filter', { track: '#Trump' },
         function(stream) {
  
         stream.on('data', function( tweet ) {
             var tweet_id = tweet.id_str;
             var tweet_text = tweet.text;
-            var name = tweet.user.name;
-            console.log(name);
-            console.log(tweet_text);
-            socket.emit('tweet', tweet);
+            io.sockets.emit('tweet', tweet);
         });
  
         stream.on('error', function ( error ) {
